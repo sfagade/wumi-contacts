b748bbac749ec7e4afec716d66dcdcebf2fc1cf9
sfagade <sfagade@gmail.com>
2022-11-26 18:59:30 +0000

    Finished displaying contacts on home

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,6 +1,8 @@
+let contacts_list;
+let last_id;
+
 function init() {
-    let contacts_list = JSON.parse(localStorage.getItem('wumi_contacts'));
-    console.log("contacts: ", contacts_list)
+    contacts_list = JSON.parse(localStorage.getItem('wumi_contacts'));
 
     if (!contacts_list) {
         $.getJSON('startup/init_data.json', function(contact_json) {
@@ -9,8 +11,24 @@ function init() {
             saveDataToLocalStorage(contacts_list);
         });
     }
+
+    displayAllContacts();
 }
 
 function saveDataToLocalStorage(contact_list) {
     localStorage.setItem('wumi_contacts', JSON.stringify(contact_list));
+}
+
+function displayAllContacts() {
+    if (contacts_list) {
+        contacts_list.forEach(contact => {
+            $('#contacts_ul')
+                .append('<li class="list-group-item"><a href="src/pages/details.html?id='+contact.id+'">'
+                            +contact.firstName+" "+contact.lastName+
+                        '</a></li>');
+            last_id = contact.id;
+        });
+    } else {
+        console.log("Contact list is empty")
+    }
 }
