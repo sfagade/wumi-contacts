12070a21b673bb6b707680fff1789ceaddb45e4e
sfagade <sfagade@gmail.com>
2022-12-21 19:50:20 +0000

    Setup main layout component and integrated it

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
-import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+function OlaApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default OlaApp
