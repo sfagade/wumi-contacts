58f35150acaac9665ab2b6c04a20b6cdb6296818
sfagade <sfagade@gmail.com>
2022-12-22 17:01:20 +0000

    Added headers & started work on contacts-display

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,51 @@
 import Head from 'next/head'
 import Layout from '../components/layout'
 import Container from '../components/container'
+import Header from '../components/header'
+import ContactsDisplay from '../components/contacts-display'
 
-export default function Home({preview}) {
+export default function Home({contacts_list}) {
   return (
     <>
-      <Layout preview={preview}>
+      <Layout>
         <Head>
           <title>Wumi's Contact Manager</title>
           <link href="/dist/output.css" rel="stylesheet"></link>
         </Head>
+        <Header />
         <Container>
-          <div className="text-3xl font-bold underline">This is my content!!</div>
+          <ContactsDisplay contacts={contacts_list} />
         </Container>
       </Layout>
     </>
   )
+}
+
+export async function getServerSideProps() {
+
+  console.log('Static props called')
+  let contacts_list = [
+    {
+      "id": 1,
+      "fullName": "Adam Mckay",
+      "firstName": "Adam",
+      "lastName": "Mckay",
+      "phoneNumber": "+44703493283",
+      "emailAddress": "adam.mckay@office.com"
+    },
+    {
+      "id": 2,
+      "fullName": "Kelvin McLoad",
+      "firstName": "Kelvin",
+      "lastName": "McLoad",
+      "phoneNumber": "+447034933253",
+      "emailAddress": "kelwin.mclord@office.com"
+    }
+  ]
+
+  return {
+    props: {
+      contacts_list
+    }, // will be passed to the page component as props
+  }
 }
